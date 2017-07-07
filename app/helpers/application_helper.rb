2556ebb8bc1ddfea9176cd7128648e9b402ffeca
Daniel Ball <g.thirteen333@gmail.com>
2017-07-07 01:13:05 +0000

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # Returns the full title on a per-page basis.
+  
+  
+  def full_title(page_title = '')
+    base_title = "About 4K TV"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
