579b3cc21bb010880ad0bbdb2042c226fb6fecb3
Daniel Ball <g.thirteen333@gmail.com>
2017-07-07 00:14:15 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @base_title = "About 4K TV"
+  end
+  
   test "should get root" do
     get root_url
     assert_response :success
@@ -10,13 +14,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | About 4K TV"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | About 4K TV"
+    assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
