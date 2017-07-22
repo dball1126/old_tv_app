b93435ea831e97c455f849f34865faf05337ff27
Daniel Ball <g.thirteen333@gmail.com>
2017-07-22 22:15:57 +0000

    Finished adding links and editing for mobile

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,11 +7,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     
       get root_path
-      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", root_path, count: 3
       assert_select "a[href=?]", about_path
       assert_select "a[href=?]", contact_path
       assert_select "a[href=?]", bit_rate_path
       assert_select "a[href=?]", file_size_path
+      assert_select "a[href=?]", hdr_path
+      assert_select "a[href=?]", pixel_chart_path
+      assert_select "a[href=?]", how_4k_path
+      assert_select "a[href=?]", i_vs_p_path
+      assert_select "a[href=?]", ultra_hd_path
+      assert_select "a[href=?]", pixel_chart_path
+      assert_select "a[href=?]", what_is_4k_path
+      assert_select "a[href=?]", where_4k_path
+      assert_select "a[href=?]", shortcuts_path
       get contact_path
       assert_select "title", full_title("Contact")
   end
